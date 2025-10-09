281326a1eb9996c6e0c153c5a9704ba51aae231f
hodges-troy <tjhodges12@gmail.com>
2025-10-08 17:14:00 -0700

    Create week_two.py

---
+++ week_two/week_two.py
@@ -0,0 +1,13 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    # Question 1: There's one column with missing values. What is it?
+    eff = pd.read_csv("week_one/car_fuel_efficiency.csv")
+    na_cols = eff.columns[eff.isna().sum() > 0]
+    logger.info(f"Cols with nulls: {na_cols}")
